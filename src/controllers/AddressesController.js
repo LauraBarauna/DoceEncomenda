b81b8d8061c65b69f37044e9b4f80c74cdde8b99
Laura Barauna <lauravbarauna@gmail.com>
2025-02-10 19:34:41 -0300

    feat: Create AddressesController and set up initial configuration

---
+++ src/controllers/AddressesController.js
@@ -0,0 +1,8 @@
+const adressesModel = require('../models/AddressesModel');
+
+function AddressesController() {
+
+}
+
+const addressesController = new AddressesController();
+module.exports = addressesController;
