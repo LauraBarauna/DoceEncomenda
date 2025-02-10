3bb22a163bb3550460989fc81a79b1b3c867d0c1
Laura Barauna <lauravbarauna@gmail.com>
2025-02-10 19:32:49 -0300

    feat: Add createAddress method to AddressesModel

--- src/models/AddressesModel.js
+++ src/models/AddressesModel.js
@@ -1,7 +1,17 @@
 const db = require('../database/db');
 
 function AdressesModel() {
+  this.createAddress = async function (label, street, city, state, complement, number, cep, phone, clientId) {
+    const sql = 'INSERT INTO addresses (label, street, city, state, complement, number, cep, phone, client_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
+    try {
+      const result = await db.execute(sql, [label, street, city, state, complement, number, cep, phone, clientId]);
+      return result;
+    } catch (error) {
+      throw new Error('Error creating address: ' + error.message);
+    }
+
+  }
 };
 
 const adressesModel = new AdressesModel();
