734ac369004c1f1bf58fba484a48ff9ca06d4daa
Laura Barauna <lauravbarauna@gmail.com>
2025-02-10 19:45:49 -0300

    feat: Add store method to AddressesController

--- src/controllers/AddressesController.js
+++ src/controllers/AddressesController.js
@@ -1,7 +1,25 @@
 const adressesModel = require('../models/AddressesModel');
 
 function AddressesController() {
+  this.store = async function (req, res) {
+    const { client_id } = req.params;
+    const { label, street, city, state, complement, number, cep, phone } = req.body;
 
+    if(!label || !street || !city || !state || !cep || !phone) {
+      return res.status().json({error: `Fields: Label, street, city, state, cep and phone are requerid!`});
+    };
+
+    if (complement === undefined) complement = null;
+    if (number === undefined) number = null;
+
+    try {
+      const newAddress = await adressesModel.createAddress(label, street, city, state, complement, number, cep, phone, client_id);
+      res.status(201).json({ message: `Address ${label} created successfully!` });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+  }
 }
 
 const addressesController = new AddressesController();
