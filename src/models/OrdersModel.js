2b9d3d87e1f8162fda3be976318f437710673d9d
Laura Barauna <lauravbarauna@gmail.com>
2025-02-13 17:08:24 -0300

    feat: Add createOrder method to OrdersModel

--- src/models/OrdersModel.js
+++ src/models/OrdersModel.js
@@ -1,7 +1,18 @@
 const db = require('../database/db');
 
 function OrdersModel () {
+  this.createOrder = async function (label, sweet_type, flavor, quantity, filling, allergens, special_request, payment_method, delivery_date, client_id, address_id, client_id, address_id) {
+    const sql = 'INSERT INTO orders (label, sweet_type, flavor, quantity, filling, allergens, special_request, payment_method, delivery_date, client_id, address_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
+    try {
+      const order = await db.execute(sql, [label, sweet_type, flavor, quantity, filling, allergens, special_request, payment_method, delivery_date, client_id, address_id]);
+      return order;
+
+    } catch (error) {
+      throw new Error('Error creating order: ' + error.message);
+    }
+
+  }
 }
 
 const ordersModel = new OrdersModel();
